33f457cb3112f6be104b35c373108ed72564a619
briancatraguna <briancatraguna@rocketmail.com>
2021-07-10 23:55:26 +0700

    Connection with spotify API workedgit add .

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+const SPOTIFY_TESTING_URL = "https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=5";
+
+const getSpotifySearch = async () => {
+  try {
+    const response = await axios.get(`${SPOTIFY_TESTING_URL}`,{
+      headers: {
+        'Authorization': process.env.REACT_APP_OAUTH_TOKEN,
+        'Accept': "application/json",
+        'Content-Type': "application/json"
+      }
+    })
+    const sampleResult = response.data.artists.items[0].id;
+    console.log(`Here's an example axios GET: ${sampleResult}`);
+  } catch(errors){
+    console.error(errors);
+  }
+}
+
+getSpotifySearch();
 
 console.log(process.env.REACT_APP_CLIENT_ID);
 console.log(process.env.REACT_APP_OAUTH_TOKEN);
