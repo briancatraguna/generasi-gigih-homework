092ca11addef7ee2ac65cfe13e28d162fa76c130
briancatraguna <briancatraguna@rocketmail.com>
2021-07-15 16:34:00 +0700

    Refactored the song item into a react component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,10 @@ import './../src/App.css';
 import 'regenerator-runtime/runtime';
 import data from './data/albumData.js';
 import TopNavigationMenu from './components/TopNavigationMenu/index.jsx';
-import SectionTitle from './components/SectionTitle/index.jsx'
+import SectionTitle from './components/SectionTitle/index.jsx';
+import SongItem from './components/SongItem/index.jsx'
 
 console.log(process.env.REACT_APP_CLIENT_ID);
-
-function showAlert(){
-  alert(`You selected ${data.album.name} !`)
-}
 
 function App() {
   return (
@@ -19,12 +16,13 @@ function App() {
       <TopNavigationMenu name="Radio"/>
     
       <SectionTitle title="Songs you might like"/>
-      <div className="itemContainer">
-        <img src={data.album.images[0].url} alt="image"/>
-        <p className="songTitle">{data.album.name}</p>
-        <p className="artist">{data.album.artists[0].name}</p>
-        <button className="selectButton" onClick={showAlert}>Select</button>
-      </div>
+      <SongItem imgUrl={data.album.images[0].url} songTitle={data.album.name} artist={data.album.artists[0].name}/>
+      {/* <div className="itemContainer">
+                <img src={data.album.images[0].url} alt="image"/>
+                <p className="songTitle">{data.album.name}</p>
+                <p className="artist">{data.album.artists[0].name}</p>
+                <button className="selectButton" onClick={showAlert}>Select</button>
+            </div> */}
     </div>
   );
 }
--- my-app/src/components/SongItem/index.jsx
+++ my-app/src/components/SongItem/index.jsx
@@ -1,2 +1,24 @@
 import React from 'react';
 import './style.css';
+
+class SongItem extends React.Component {
+    constructor(props){
+        super(props);
+        this.showAlert = this.showAlert.bind(this)
+    }
+
+    showAlert(){
+        alert(`You selected ${this.props.songTitle} !`)
+      }
+
+    render(){
+        return <div className="itemContainer">
+                <img src={this.props.imgUrl} alt="image"/>
+                <p className="songTitle">{this.props.songTitle}</p>
+                <p className="artist">{this.props.artist}</p>
+                <button className="selectButton" onClick={this.showAlert}>Select</button>
+            </div>
+    }
+}
+
+export default SongItem;
