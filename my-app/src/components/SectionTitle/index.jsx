3964f91e5afec1bf591e623801629060e3f62160
briancatraguna <briancatraguna@rocketmail.com>
2021-07-23 00:06:36 +0700

    Refactored section title

--- my-app/src/components/SectionTitle/index.jsx
+++ my-app/src/components/SectionTitle/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './style.css';
 
-class SectionTitle extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return <h1 className="songsTitle">{this.props.title}</h1>
-    }
+const SectionTitle = (props) => {
+    return (
+        <h1 className="songsTitle">{props.title}</h1>
+    );
 }
 
 export default SectionTitle;
