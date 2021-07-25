66c091774968ffec57a915451267c3073e9e7be7
briancatraguna <briancatraguna@rocketmail.com>
2021-07-25 21:01:36 +0700

    Added uri

--- my-app/src/components/LoginButton/index.jsx
+++ my-app/src/components/LoginButton/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './style.css';
 
-const REDIRECT_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=http://localhost:3000/callback&scope=playlist-modify-private`
+const MY_WEBSITE = "https://spotifynya-brian.vercel.app/"
+const REDIRECT_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=${MY_WEBSITE}&scope=playlist-modify-private`
 
 const LoginButton = () => {
     return(
