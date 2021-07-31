2fbb49179de38bf2859c6d0f646ee50f0fc0e770
briancatraguna <briancatraguna@rocketmail.com>
2021-07-31 20:27:40 +0700

    Added router

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,15 +3,19 @@ import 'regenerator-runtime/runtime';
 import HomePage from './pages/HomePage.jsx';
 import configureStore from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Provider store={configureStore}>
-        <HomePage></HomePage>
-      </Provider>
-    </div>
+    <Router>
+      <div className="App">
+        <Provider store={configureStore}>
+          <HomePage></HomePage>
+        </Provider>
+      </div>
+    </Router>
+    
   );
 }
 
